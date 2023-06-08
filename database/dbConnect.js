80be7761a964265671b7e8862b42e34c52d5c721
Dakshay Bishnoi <dakshaybishnoi@gmail.com>
2023-06-08 10:22:26 +0530

    added some changes

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDb = ()=>{
-  mongoose.connect(process.env.DB_URI,{
-    dbName:"masterclass_node"
-  }).then(()=>{
+  mongoose.connect(process.env.DB_URI).then(()=>{
     console.log("Database connected successfully.");
   }).catch((e)=>{
       console.log(e);
